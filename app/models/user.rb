ddf498f0d30811331fc85d0137ffeedab8e0978b
tenndou8110 <139302819+tenndou8110@users.noreply.github.com>
2023-08-07 20:10:32 +0900

    ユーザー管理機能の実装課題

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # メールアドレスは必須であり、Deviseのデフォルトのバリデーションを使用
+  validates :email, presence: true
+
+  # パスワードは必須であり、Deviseのデフォルトのバリデーションを使用
+  validates :encrypted_password, presence: true
+
+  # ユーザー名は必須
+  validates :name, presence: true
+
+  # プロフィールは必須
+  validates :profile, presence: true
+
+  # 所属は必須
+  validates :occupation, presence: true
+
+  # 役職は必須
+  validates :position, presence: true
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'prototypes/index'
   get 'prototypes/index' => 'prototypes#index'
 end
