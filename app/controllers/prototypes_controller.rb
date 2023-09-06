e6a2a78849ed73439977a62e2c8dcb1a6a4fa2a1
tenndou8110 <139302819+tenndou8110@users.noreply.github.com>
2023-09-06 15:46:31 +0900

    修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,14 +1,23 @@
 class PrototypesController < ApplicationController
-  before_action :contributor_confirmation, only:[:edit, :destroy, :update]
-  before_action :authenticate_user!, except: [:index, :show,]
   before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
+  def index
+    @prototypes = Prototype.all
+  end
 
   def new
     @prototype = Prototype.new
   end
 
-  def index
-    @prototypes = Prototype.all
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -18,7 +27,7 @@ class PrototypesController < ApplicationController
 
   def edit
   end
-  
+
   def update
     if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
@@ -26,21 +35,13 @@ class PrototypesController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
-  
+
   def destroy
     if @prototype.destroy
       redirect_to root_path
     else
       redirect_to root_path
     end
-
-  def create
-    @prototype = Prototype.new(prototype_params)
-    if @prototype.save
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end  
   end
 
   private
@@ -48,7 +49,6 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
 
   def set_prototype
     @prototype = Prototype.find(params[:id])
