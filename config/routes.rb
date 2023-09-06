0407a1db9da27af34e65bb8e6e50e296206af13d
tenndou8110 <139302819+tenndou8110@users.noreply.github.com>
2023-09-06 15:30:45 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
     resources :comments, only: :create
   end
   root to: 'prototypes#index'
-  resources :users, only: [:show]
+  resources :users, only: :show
 end
