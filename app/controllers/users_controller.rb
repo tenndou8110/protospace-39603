87384d32e93bcbcdbe9e6d1321f1f1f00e6cd5b1
tenndou8110 <139302819+tenndou8110@users.noreply.github.com>
2023-09-08 12:00:40 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @name = user.name
-    @prototype = user.prototype
-    @occupation = user.occupation
-    @position = user.position
   end
 end
