802b21aed0c6d289c7bc09218ea642107dd330b0
tenndou8110 <139302819+tenndou8110@users.noreply.github.com>
2023-09-08 18:24:52 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,13 +6,13 @@ class CommentsController < ApplicationController
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
-      render "prototype/show"
+      render "prototypes/show"
     end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id,prototype_id: params[:prototype_id])
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
 end
